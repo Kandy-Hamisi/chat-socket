b0943c938d8673b7756af4b425e5a7004925692a
Kandy-Hamisi <hkandy2000@gmail.com>
2022-10-19 12:23:15 +0300

    :heavy_plus_sign: UPDATE: added redux for permanent message storage in localStorage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import { persistStore } from 'redux-persist'
+import store from '../app/store'
 import './index.css'
+
+let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </>
 )
